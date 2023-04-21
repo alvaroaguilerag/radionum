84bc449024f9dde9d2e7feefa11b01bebe9bad45
alvaroaguilerag <al.aguilerag@duocuc.cl>
2023-04-21 02:40:34 +0000

    Se crea código en python para calcular el radio de una circunferencia

---
+++ radio8.py
@@ -0,0 +1,6 @@
+from math import pi
+
+print("Valor de circunferencia de radio del número 8")
+radio = float(8)
+circunferencia = 2 * pi * radio
+print(f"La circunferencia de radio {radio} es: {circunferencia:.6f}")
